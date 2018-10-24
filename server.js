7d3329e36eef295409cdcd6c250c1bf0a5356684
PremKumar <30384028+uiinterpreter@users.noreply.github.com>
2018-10-24 08:58:36 +0530

    made changes for deployment

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/perspective'));
+app.use(express.static(__dirname + '/dist/movie-search-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/perspective/index.html'));
+res.sendFile(path.join(__dirname+'/dist/movie-search-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
