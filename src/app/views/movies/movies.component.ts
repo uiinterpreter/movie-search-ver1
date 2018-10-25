002ad3a8f244921abb84f8b2fba842955726203a
PremKumar <30384028+uiinterpreter@users.noreply.github.com>
2018-10-25 14:44:28 +0530

    added session storage

--- src/app/views/movies/movies.component.ts
+++ src/app/views/movies/movies.component.ts
@@ -15,6 +15,8 @@ export class MoviesComponent implements OnInit {
   public view:string='trending';
   public pageNo:number=1;
   public total:number;
+  public total_count:number;
+  public searchQuery:string;
   constructor(private getMovie:GetMovieService,
               private  route:Router,
               private avtivated:ActivatedRoute) { }
@@ -22,22 +24,34 @@ export class MoviesComponent implements OnInit {
   ngOnInit() {
     this.date = new Date();
     this.date = `${this.date.getFullYear()}-${this.date.getMonth()}-${this.date.getDate()}`;
-    this.getTrendingMovies();
+    let type = window.sessionStorage.getItem('viewType');
+    let query = window.sessionStorage.getItem('searchQuery');
+    let pageNo = window.sessionStorage.getItem('pageNo');
+    this.view  = type || 'trending';
+    this.searchQuery  = query || '';
+    this.pageNo = parseInt(pageNo) || 1;
+    this.getTrendingMovies(this.view);
   }
 
-  getTrendingMovies(){
-    this.loader = true;
-    this.serviceObj = {
-      method:1,
-      pageNo:this.pageNo,
-    };
+  getTrendingMovies(type:string){
     try{
+      this.loader = true;
+      this.serviceObj = {
+        method:1,
+        pageNo:this.pageNo,
+        type:type,
+        query:this.searchQuery
+      };
+      window.sessionStorage.setItem('viewType',this.view);
+      window.sessionStorage.setItem('searchQuery',this.searchQuery);
+      window.sessionStorage.setItem('pageNo',JSON.stringify(this.pageNo));
       this.getMovie.getMovies(this.serviceObj)
       .subscribe(
         (response: any) => {
             this.loader = false;
             this.cardS = response.results;
             this.total = response.total_pages;
+            this.total_count = response.total_results;
         },
         (error) => {console.log(error);
           this.route.navigate(['/error']);
@@ -51,15 +65,27 @@ export class MoviesComponent implements OnInit {
   pageChange(val:boolean){
     if(val && this.pageNo < this.total){
       this.pageNo +=1;
-      this.getTrendingMovies();
+      this.getTrendingMovies(this.view);
     }else if(!val && this.pageNo > 1){
       this.pageNo -= 1;
-      this.getTrendingMovies();
+      this.getTrendingMovies(this.view);
     }
   }
 
-  movieSearch(search:string){
-    console.log(search);
+  showTrending(){
+    this.view = 'trending';
+    window.sessionStorage.setItem('searchQuery',undefined);
+    this.getTrendingMovies(this.view);
+  }
+
+  movieSearch = (search:string) => {
+    try{
+      this.searchQuery = search;
+      this.view = 'search';
+      this.getTrendingMovies(this.view);
+    }catch(e){
+      console.error(e);
+    }
   }
 
 }
