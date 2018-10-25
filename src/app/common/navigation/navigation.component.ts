a279486faabdc9b2c57d4536a03505060659ad12
PremKumar <30384028+uiinterpreter@users.noreply.github.com>
2018-10-25 14:37:57 +0530

    added session storage

--- src/app/common/navigation/navigation.component.ts
+++ src/app/common/navigation/navigation.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit, OnChanges,Input } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import {FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit,OnChanges {
+export class NavigationComponent implements OnInit {
   @Input() clickEvent:any;
   public searchQuery:string='';
   constructor() { }
 
   ngOnInit() {
-  }
-
-  ngOnChanges(){
+    let query = window.sessionStorage.getItem('searchQuery');
+    let type = window.sessionStorage.getItem('viewType');
+    if(type && type === 'search'){
+      this.searchQuery  = query || '';
+    }
   }
   
-  searchMovie(){
+  searchMovie(event?:any){
     try{
-      this.clickEvent(this.searchQuery);
+      if(event.keyCode === 13 || event.type == "click"){
+        this.clickEvent(this.searchQuery);
+      }
     }catch(e){
       console.error(e);
     }
