f8fa08ce8974302432a3e83db30d3234e02ee866
uiinterpreter <premp4920@gmail.com>
2018-10-29 22:35:51 +0530

    fixed bug

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-var request = require('request');
+// var request = require('request');
  
 const app = express();
 
@@ -12,15 +12,15 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/movie-search-app/index.html'));
 });
 
-app.route('/api/movie').get((req, res) => {
+// app.route('/api/movie').get((req, res) => {
 
-});
-app.route('/api/movies').get((req, res) => {
+// });
+// app.route('/api/movies').get((req, res) => {
 
-});
-app.route('/api/details').get((req, res) => {
+// });
+// app.route('/api/details').get((req, res) => {
 
-});
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
