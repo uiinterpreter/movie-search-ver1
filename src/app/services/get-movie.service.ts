a56ec31431cb35c5f331cf579c0f0140299a8ffd
PremKumar <30384028+uiinterpreter@users.noreply.github.com>
2018-10-31 14:30:43 +0530

    date bug

--- src/app/services/get-movie.service.ts
+++ src/app/services/get-movie.service.ts
@@ -24,7 +24,7 @@ export class GetMovieService {
     try{
       let date:any = new Date();
       date = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
-      if (request.type === 'trending') return `https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${date}&api_key=1bcb0de5947ee4ccfdd236151dc0f99d&sort_by=popularity.desc&page=${request.pageNo}`;
+      if (request.type === 'trending') return `https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2018-9-30&api_key=1bcb0de5947ee4ccfdd236151dc0f99d&sort_by=popularity.desc&page=${request.pageNo}`;
       
       else if (request.type === 'search') return `https://api.themoviedb.org/3/search/movie?api_key=1bcb0de5947ee4ccfdd236151dc0f99d&sort_by=popularity.desc&page=${request.pageNo}&query=${request.query}`;
       
