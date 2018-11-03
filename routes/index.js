f6954eec41258510f6e418aa2873581ab8ad1372
uiinterpreter <premp4920@gmail.com>
2018-11-03 19:58:49 +0530

    added responsive changes and search changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,32 @@
-import {Router,Request,Response,NextFunction} from 'express';
-import * as request from 'request';
+// import {Router} from 'express';
+// import * as request from 'request';
+// import {apiEndPOints} from './config';
+// import { pipe } from 'rxjs';
 
-// const apiEndPoints = {
-// trending:`${}`
-// };
+const router = require('express');
+const request  = require('request');
+// const apiEndPoints = require('./config');
+const pipe = require('rxjs');
+const apiEndPOints = {
+    search:'https://api.themoviedb.org/3/search/movie',
+    trending:'https://api.themoviedb.org/3/discover/movie',
+    details:'https://api.themoviedb.org/3/movie/'
+}
+const api = router();
+
+api.use((req,res,next) => {
+    console.log('Movie search app request by action :' + req.url )
+    next()
+})
+
+api.get('/api/trending', (req,res) => {
+    const tempArr =[];
+    let str='';
+    for(const key in req.query){
+        tempArr.push(`${key}=${req.query[key]}`);
+    }
+    str = tempArr.join('&');
+    request.get(`${apiEndPoints.trending}?${(str)}`).pipe(res)
+})
+
+// export default api
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
+// import * as movieApi from '../routes/index'
+
 const express = require('express');
 const path = require('path');
-
+const movieApi= require('./routes/index');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/movie-search-app'));
 
+app.use(express.static(__dirname + '/dist/movie-search-app'));
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/movie-search-app/index.html'));
 });
 
-// app.route('/api/trending').get((req, res) => {
-   
-// });
+// app.route('/api/trending',movieApi);
 // app.route('/api/movies').get((req, res) => {
 
 // });
