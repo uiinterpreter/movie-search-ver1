cee852031f77e42f344c2c092a87e82e9ca84650
PremKumar <30384028+uiinterpreter@users.noreply.github.com>
2019-01-13 10:52:39 +0530

    changed month

--- src/app/services/get-movie.service.ts
+++ src/app/services/get-movie.service.ts
@@ -23,7 +23,7 @@ export class GetMovieService {
   formAPIString(request:any){
     try{
       let date:any = new Date();
-      date = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+      date = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
       if (request.type === 'trending') return `https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${date}&api_key=1bcb0de5947ee4ccfdd236151dc0f99d&sort_by=popularity.desc&page=${request.pageNo}`;
       // if (request.type === 'trending') return `/api/trending?primary_release_date.gte=${date}&api_key=1bcb0de5947ee4ccfdd236151dc0f99d&sort_by=popularity.desc&page=${request.pageNo}`;
 
